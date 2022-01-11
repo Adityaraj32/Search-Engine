57460cfe11e9ecc437da0fad90473832763cbfa9
Aditya raj <87028110+Adityaraj32@users.noreply.github.com>
2022-01-11 22:04:08 +0530

    Add files via upload

---
+++ Python Practise_7.py
@@ -0,0 +1,8 @@
+'''
+Author: Adityaraj Yadav 
+Date: 11 January 2022
+Project Name: Search Engine
+Purpose:For Practising Purpose
+'''
+query = input("Search With Aditya's Search Engine: ")
+codewithharry.get()
